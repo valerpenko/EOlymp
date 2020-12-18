5bc659fa3dcd7adf4ad4d1b6bfd5ae5cd18b0798
valerpenko <vpenko@onu.edu.ua>
2020-12-18 17:57:06 +0200

    3dMatches

---
+++ _3dMatches.py
@@ -0,0 +1,40 @@
+import math
+def matchesForUpgrade(n_rest,width,height):
+    # returns
+    # 1 match count for upgrade to build n_rest cubes
+    # 2 count of remain cubes
+    matches=0
+    for row in range(height):
+        for col in range(width):
+            if row==0:
+                if col==0:
+                    matches+=8
+                else:
+                    matches+=5
+            else:
+                if col==0:
+                    matches+=5
+                else:
+                    matches+=3
+            n_rest-=1
+            if n_rest==0:
+                return matches, 0
+    return matches, n_rest
+
+n=int(input())  #количество кубиков
+k=math.floor(math.pow(n,1./3.))
+m_kcube=3*(k+1)*(k+1)*k
+m_level=0
+m_back=0
+m_facade=0
+n_rest=n-k*k*k
+if n_rest>0:  # next level
+    m_level, n_rest= matchesForUpgrade(n_rest, k, k)
+    if  n_rest>0: #   next back
+        m_back, n_rest = matchesForUpgrade(n_rest, k, k+1)
+        if  n_rest>0: # next facade
+            m_facade, n_rest = matchesForUpgrade(n_rest, k+1, k + 1)
+m=m_kcube+m_level+m_back+m_facade
+print(m)
+
+
