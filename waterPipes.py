07885e3de8b704b83dc13cca4fc4d52dedc9084b
valerpenko <vpenko@onu.edu.ua>
2021-01-08 17:54:24 +0200

    Pipeline recursive prototype

--- waterPipes.py
+++ waterPipes.py
@@ -21,21 +21,45 @@
 #
 # print(connections)
 # print(swapPipes(connections))
+#==============================================================
+# connections = []
+#
+# def swapPipes(con):
+#     result = [(pipe[1], pipe[0]) for pipe in con]
+#     return result
+#
+# def Pipeline(distNum):
+#     for i in range(1, distNum + 1):
+#         if i != i + 1 and i < i + 1:
+#                 connections.append((i, i + 1))
+#     #[connections.append((i, i + 1)) for i in range(1, distNum + 1) if i != i + 1 and i < i + 1]
+#
+# districtNum = int(input())
+#
+# Pipeline(districtNum)
+# print(connections)
+# print(swapPipes(connections))
+#===============================================
 
-connections = []
+def GoodCity():
+    for srcRegion in city:
+        for region1 in city:
+            region1==srcRegion:
+            continue
+            visitList=[]
+            if !region1.HasConnectionWith(srcRegion):
+                return false
+    return true
 
-def swapPipes(con):
-    result = [(pipe[1], pipe[0]) for pipe in con]
-    return result
 
-def Pipeline(distNum):
-    for i in range(1, distNum + 1):
-        if i != i + 1 and i < i + 1:
-                connections.append((i, i + 1))
-    #[connections.append((i, i + 1)) for i in range(1, distNum + 1) if i != i + 1 and i < i + 1]
+def HasConnectionWith(srcRegion):
+    if self in visitList:
+        return false
+    else:
+        visitList.Append(self)
+        if self.DirectConnectedWith(srcRegion):
+            return true
+        else:
+            for region in self.SrcConnections:
+                return region.HasConnectionWith(srcRegion)
 
-districtNum = int(input())
-
-Pipeline(districtNum)
-print(connections)
-print(swapPipes(connections))
