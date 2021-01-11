3991c9db18eea003324b16d9340c8dd56f66b406
valerpenko <vpenko@onu.edu.ua>
2021-01-11 17:02:45 +0200

    Fast Fibonacci accordingly eolymp

--- fastFibonacci.py
+++ fastFibonacci.py
@@ -1,40 +1,21 @@
-
 def calculateFibonacci(num):
     global k
-    k = k + 1
-    #k = 0
     if 1 <= num <= 4:
-        if num in fibRepo.keys():
-            return fibRepo[num]
-        else:
-            fibRepo[num]=1
+            #return fibRepo[num-1]
             return 1
     else:
-
-        if num in fibRepo.keys():
-            return fibRepo[num]
+        if  num<=len(fibRepo):
+            return fibRepo[num-1]
         else:
-            fibRepo[num]=(calculateFibonacci(num - 1) + calculateFibonacci(num - 2)+ calculateFibonacci(num - 3) + calculateFibonacci(num - 4))
-            return fibRepo[num]
+            fibRepo.append((calculateFibonacci(num - 1) + calculateFibonacci(num - 2)+ calculateFibonacci(num - 3) + calculateFibonacci(num - 4)))
+            return fibRepo[num-1]
 
-def digitLimil(lim):
-    string = str(lim)
-    if len(string) <= 2008:
-        return True
-k=0
-fibRepo=dict()
-#inp = 40
-inp = int(input())
-list = [calculateFibonacci(x) for x in range(1,inp + 1)]
-# print(list)
-# print(k)
-
-countList = {count:fibNum for count, fibNum in enumerate(list,1)}
-print(countList)
-
+fibRepo=[1,1,1,1]
 n = int(input())
-search = input().split()[:n]
-#search = input().split()
-for num in search:
-    print(countList[int(num)])
+indexes=[]
+for i in range(n):
+    indexes.append(int(input()))
+for index in indexes:
+    fib=calculateFibonacci(index)
+    print(fib)
 
