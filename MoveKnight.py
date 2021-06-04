807adda8a893b3fd28c16bc9dc7ef3fe351d8c33
John <ilya92702@gmail.com>
2021-06-04 16:10:29 +0300

    WaterPipes reconstruct

---
+++ MoveKnight.py
@@ -0,0 +1,3 @@
+
+chessBoard = [[0 for i in range(8)] for j in range(8)]
+print(chessBoard)
--- WaterPipes.py
+++ WaterPipes.py
@@ -1,39 +1,68 @@
-def DefinePipes():
-    city = [[True,True,True,True],
-            [False,True,True,True],
-            [False,False,True,True],
-            [False,False,False,True]]
-    return city
-
-def Reconstruct(city, region):
-    newCity = [list(x) for x in zip(*city)]   # different way
-    return newCity
-
-# def SupplyTest():
-
-def Pipes(city):
-    for region in range(len(city)):
-        newCity = Reconstruct(city, region)
-        testActive = True
-        for activeRegion in range(len(city)):
-            if not SupplyTest(activeRegion):
-                testActive = False
-                break
-        if testActive:
-            return True
-    return False
-
-city = DefinePipes()
-if Pipes(city):
-    print(1)
-else:
-    print(0)
-
-
-# city = [[True,True,True,True],
-#         [False,True,True,True],
-#         [False,False,True,True],
-#         [False,False,False,True]]
+# def DefinePipes():
+#     city = [[True,True,True,True],
+#             [False,True,True,True],
+#             [False,False,True,True],
+#             [False,False,False,True]]
+#     return city
 #
-# result = [list(x) for x in zip(*city)]
-# print(result)
+# def Reconstruct(city, regNum):
+#     newCity = DefinePipes()
+#     temp = newCity[regNum]
+#     column = []
+#     for row in city:
+#         column.append(row[regNum])
+#     newCity[regNum] = column
+#     # newCity[regNum + 1] = temp
+#     return newCity
+#
+# def SupplyTest(region):
+#
+#
+# def Pipes(city):
+#     for region in range(len(city)):
+#         newCity = Reconstruct(city, region)
+#         testActive = True
+#         for activeRegion in range(len(city)):
+#             if not SupplyTest(activeRegion):
+#                 testActive = False
+#                 break
+#         if testActive:
+#             return True
+#     return False
+#
+# city = DefinePipes()
+# if Pipes(city):
+#     print(1)
+# else:
+#     print(0)
+#
+#
+# # city = [[True,True,True,True],
+# #         [False,True,True,True],
+# #         [False,False,True,True],
+# #         [False,False,False,True]]
+#
+#
+# # def reconstruct(city, regNum):
+# #     newCity = city
+# #     temp = newCity[regNum]
+# #     column = []
+# #     for row in city:
+# #         column.append(row[regNum])
+# #     newCity[regNum] = column
+# #     newCity[regNum + 1] = temp
+# #     return newCity
+#
+#
+#
+# # def reconstruct(city, regNum):
+# #     newCity = city
+# #     for i in range(len(newCity[regNum])):
+# #         for j in range(len(newCity)):
+# #             newCity[i][j], newCity[j][i] = newCity[j][i], newCity[i][j]
+# #             return newCity
+# #
+# # print(reconstruct(city,0))
+#
+# # result = [list(x) for x in zip(*city)]
+# # print(result)
