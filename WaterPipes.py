904e92a1cf314bece7f0b24c3347d2f0eaf5ab6e
valerpenko <vpenko@onu.edu.ua>
2021-06-28 18:07:14 +0300

    Pipes seems finished

--- WaterPipes.py
+++ WaterPipes.py
@@ -18,42 +18,40 @@ def Reconstruct(city, regNum):
         newCity[i][regNum],newCity[regNum][i]=newCity[regNum][i],newCity[i][regNum]
     return newCity
 
-def SupplyTest(city, region):
+def SupplyTest(city, region):  # True - all city water supplied     False - otherwise
     checklist = [0] * len(city)
     checklist[region] = 1
     fr = region
-    while fr != -1:
+    while True: # fr != -1:
         checklist[fr] = 2
         for i in range(len(city)):
             if city[fr][i]:
-                checklist += 1
-        fr = checklist.index(1)
-    return checklist.index(0) != -1
+                checklist[i] += 1
+        try:
+            fr = checklist.index(1)
+        except ValueError:
+            break
+    try:
+        checklist.index(0)
+        return False
+    except ValueError:
+        return True
 
-
-def Pipes(city):
-    for region in range(len(city)):
-        #newCity = Reconstruct(city, region)
-        testActive = True
+def GoodWaterPipes(city):  #True - for well water supplied city False - otherwise
         for activeRegion in range(len(city)):
             if not SupplyTest(city, activeRegion):
-                testActive = False
-                break
-        if testActive:
+                return False
+        return True
+
+def CityWaterSupplyExist(srcCity):
+    for region in range(len(srcCity)):
+        newCity=Reconstruct(srcCity, region)
+        if GoodWaterPipes(newCity):
             return True
     return False
 
 city = DefinePipes()
-if Pipes(city):
-    print(1)
+if CityWaterSupplyExist(city):
+    print(1)    #city is well water supplied
 else:
-    print(0)
-
-
-print(Reconstruct(city, 1))
-print(SupplyTest(city, 1))
-
-
-
-# result = [list(x) for x in zip(*city)]
-# print(result)
+    print(0)    #city is not water supplied
