35a02b23ad6905f66ef4ccfe9a9a475274296af8
valerpenko <vpenko@onu.edu.ua>
2021-08-30 16:17:04 +0300

    not essential

--- task№626.py
+++ task№626.py
@@ -37,13 +37,21 @@ class SimpleGraph:
       if value[0] == dest:
         return value[1]
 
+  def edgesFrom(self,src):
+    #list with [dest node,weight]
+    return self.graphDict[src]
   def show(self):
     print(self.graphDict)
 
-  # def Dijkstras_Shortest_Path(self, src):
-  #
-  #   distance = [9999999999] * self.vertCount
-  #   distance[src] = 0
+  def Dijkstras_Shortest_Path(self, src):
+    distance = [9999999999] * self.vertCount
+    distance[src] = 0
+
+    for u in self.graphDict:
+      for [v,w] in self.edgesFrom(u):
+        if distance[u] != 9999999999 and distance[u] + w < distance[v]:
+          distance[v] = distance[u] + w
+    return distance
 
   def BellmanFord(self, src):
 
@@ -87,16 +95,17 @@ class SimpleGraph:
 # graph.show()
 
 graph = SimpleGraph(directed=True)
+graph.addVertex(0)
 graph.addVertex(1)
 graph.addVertex(2)
 graph.addVertex(3)
 graph.addVertex(4)
-graph.addVertex(5)
-graph.addEdge(1, 2, 4)
-graph.addEdge(2, 1, 3)
-graph.addEdge(2, 5, 3)
-graph.addEdge(3, 2, 2)
-graph.addEdge(4, 1, 1)
-graph.addEdge(5, 3, 2)
+graph.addEdge(0, 1, 4)
+#graph.addEdge(1, 0, 3)
+graph.addEdge(1, 4, 3)
+graph.addEdge(2, 1, 2)
+graph.addEdge(3, 0, 1)
+graph.addEdge(2, 4, 2)
+graph.addEdge(2, 3, 5)
 graph.show()
-print(graph.BellmanFord(2))
+print(graph.Dijkstras_Shortest_Path(2))
